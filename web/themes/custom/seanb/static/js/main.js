83e8c930b4d7804af56e122d4b228a7f50049283
Sean B <s.p.blommaert@gmail.com>
2017-11-14 23:01:26 +0100

    Added fixes after jQuery 3.0 core update.

--- web/themes/custom/seanb/static/js/main.js
+++ web/themes/custom/seanb/static/js/main.js
@@ -5,39 +5,40 @@
       constants = Drupal.behaviors.fortytwoMain.constants;
 
       // Store responsive type
-      var body = $('body');
-      if (body.hasClass('layout-adaptive')) {
+      var $body = $('body');
+      if ($body.hasClass('layout-adaptive')) {
         constants.LAYOUT = {
           fluid: false,
           adaptive: true,
         };
       }
-      else if (body.hasClass('layout-fluid')) {
+      else if ($body.hasClass('layout-fluid')) {
         constants.LAYOUT = {
           fluid: true,
           adaptive: false,
         };
       }
 
-      $(window).once().bind('scroll', function() {
+      $(window).once().on('scroll', function() {
         var windowTop = $(window).scrollTop();
         $('header .hero-image').css({'top': (((windowTop * 0.4) * -1) - 130) + 'px'});
       });
 
       // Scroll animation for anchors.
+      var $html = $('html', context);
+      var navigation_height = $('div.navigation').height();
       $('a[href*=\\#]', context).on('click', function (event) {
         event.preventDefault();
-        var $body = $('body');
-        $($body).animate({ scrollTop: $(this.hash).offset().top - $('div.navigation').height() }, 500);
+        $html.animate({ scrollTop: $(this.hash).offset().top - navigation_height }, 500);
       });
 
       // Add class to items in viewport for loading animation.
       $('section, .skill, footer nav', context).css('opacity', '0').bind('inview', function (event, visible) {
-        if (visible == true) {
+        if (visible === true) {
           $(this).addClass('view-fade-in');
         }
       });
-    },
+    }
   };
 
 })(jQuery, Drupal, this, this.document);
