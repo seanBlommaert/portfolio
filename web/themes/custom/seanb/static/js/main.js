6662aa75ef3be384f84c4334d5458ee4c9359938
Sean Blommaert <s.p.blommaert@gmail.com>
2018-11-12 10:37:10 +0100

    Fixed some CSS style and SEO issues.

--- web/themes/custom/seanb/static/js/main.js
+++ web/themes/custom/seanb/static/js/main.js
@@ -1,30 +1,14 @@
-(function ($, Drupal, window, document, undefined) {
+(function ($, Drupal) {
 
   Drupal.behaviors.main = {
-    attach: function (context, settings) {
-      constants = Drupal.behaviors.fortytwoMain.constants;
-
-      // Store responsive type
-      var $body = $('body');
-      if ($body.hasClass('layout-adaptive')) {
-        constants.LAYOUT = {
-          fluid: false,
-          adaptive: true,
-        };
-      }
-      else if ($body.hasClass('layout-fluid')) {
-        constants.LAYOUT = {
-          fluid: true,
-          adaptive: false,
-        };
-      }
+    attach: function (context) {
 
       // Scroll animation for anchors.
       var $page = $('html, body', context);
-      var navigation_height = $('div.navigation').height();
+      var navigationHeight = $('div.navigation').height();
       $('a[href*=\\#]', context).on('click', function (event) {
         event.preventDefault();
-        $page.animate({ scrollTop: $(this.hash).offset().top - navigation_height }, 500);
+        $page.animate({ scrollTop: $(this.hash).offset().top - navigationHeight }, 500);
       });
 
       // Add class to items in viewport for loading animation.
@@ -33,7 +17,8 @@
           $(this).addClass('view-fade-in');
         }
       });
+
     },
   };
 
-})(jQuery, Drupal, this, this.document);
+})(jQuery, Drupal);
